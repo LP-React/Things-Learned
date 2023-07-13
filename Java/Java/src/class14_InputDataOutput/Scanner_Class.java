d42bafbe417e8933f0f24e587e8c8671f88f5e05
LP-React <ljamirp30@gmail.com>
2023-07-13 17:21:12 -0500

    Usando Inputs y Outputs en Java

---
+++ Java/Java/src/class14_InputDataOutput/Scanner_Class.java
@@ -0,0 +1,21 @@
+package class14_InputDataOutput;
+
+import java.util.Scanner;
+
+public class Scanner_Class {
+
+	public static void main(String[] args) {
+		
+		Scanner Input = new Scanner(System.in);
+		
+		System.out.println("Introduce tu nombre");
+		String nombre = Input.nextLine();
+		
+		System.out.println("Introduce tu edad");
+		int edad = Input.nextInt();
+
+		System.out.println("Hola " + nombre + ", el proximo año cumplirás " + (edad + 1) + " años.");
+
+	}
+
+}
---
+++ Java/Java/src/class15_InputDataOutput/JOptionPane_Class.java
@@ -0,0 +1,19 @@
+package class15_InputDataOutput;
+
+import javax.swing.*;
+
+public class JOptionPane_Class {
+
+	public static void main(String[] args) {
+		
+		String numero = JOptionPane.showInputDialog("Introduce un número, por favor");
+		
+		double edadToDouble = Double.parseDouble(numero); 
+
+		System.out.print("Tú numero es " + numero + " y su raíz cuadrada es ");
+		
+		System.out.printf( "%1.3f", Math.sqrt(edadToDouble));
+		
+	}
+
+}
