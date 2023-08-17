9691c31a4411da8d8e506e4831fe26f79d59129e
LP-React <ljamirp30@gmail.com>
2023-08-17 00:24:00 -0500

    Avanze clase 24 de Java

---
+++ Java/Java/src/class24_Matrices/Array_ForEach.java
@@ -0,0 +1,24 @@
+package class24_Matrices;
+
+public class Array_ForEach {
+	public static void main(String[] args) {
+		
+	//-----------------------------------------------------------------------------------------------------
+		
+		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int iterator : numbers) {
+			System.out.println(iterator);
+		}
+		 
+	//-----------------------------------------------------------------------------------------------------
+		
+		String names[] = {"Juan","Mateo","Smith","Ronald","Maite"};
+		
+		for (String iterador : names) {
+			System.out.println(iterador);
+		}
+		
+	}
+
+}
---
+++ Java/Java/src/class24_Matrices/Using_ForEach.java
@@ -0,0 +1,24 @@
+package class24_Matrices;
+
+import javax.swing.JOptionPane;
+
+public class Using_ForEach {
+	public static void main(String[] args) {
+
+		String[] countries = new String[5];
+
+		for (int i = 0; i < countries.length; i++) {
+			countries[i] = JOptionPane.showInputDialog("Inserta un país");
+		}
+
+		System.out.println("Lista de países insertados : ");
+		int position = 0;
+
+		for (String iterator : countries) {
+			position++;
+			System.out.println("El pais N° " + position + " es " + iterator);
+		}
+		
+	}
+
+}
